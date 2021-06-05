37cebb41059454f601fd47170d90cdfcf3960f5a
neil.ross@zonedigital.com <neil.ross@zonedigital.com>
2021-06-05 13:43:30 +0100

    first pass at basic behaviour

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
 import App from './App';
 
-test('renders learn react link', () => {
+
+// Fake timers using Jest
+beforeEach(() => {
+  jest.useFakeTimers()
+})
+
+// Running all pending timers and switching to real timers using Jest
+afterEach(() => {
+  jest.runOnlyPendingTimers()
+  jest.useRealTimers()
+})
+
+test('there is a heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole('heading')).toBeTruthy()
+
 });
+
+test('there is a start button, when clicked it becomes a pause button', () => {
+  render(<App />);
+  const startPause = screen.getByRole('button', {label: /Start/i})
+  expect(startPause).toBeTruthy()
+  userEvent.click(startPause)
+  expect(screen.getByRole('button', {label: /Pause/i})).toBeTruthy()
+
+});
+
+test('there is a timer, it starts from 25:00', () => {
+  render(<App />);
+  expect(screen.getByTestId('timer')).toBeTruthy()
+  expect(screen.getByText(/25:00/i)).toBeTruthy()
+});
+
+test('The first increment lasts', () => {
+  render(<App />);
+  const startPause = screen.getByRole('button', {label: /Start/i})
+  userEvent.click(startPause)
+
+
+});
+
+
