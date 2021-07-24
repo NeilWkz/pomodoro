dbb22ea36bd3912926ca9aa340bd918361b9f911
neil.ross@zonedigital.com <neil.ross@zonedigital.com>
2021-07-24 12:21:36 +0100

    migrate to temporal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { Temporal } from 'proposal-temporal/lib/index.mjs'
 import './App.css'
 import ClockFace from './components/ClockFace'
 
@@ -6,8 +7,18 @@ const STARTING_SECONDS = 1500
 const SHORT_BREAK = 300
 const LONG_BREAK = 900
 
+const breakDurationSequence = [
+  SHORT_BREAK,
+  SHORT_BREAK,
+  SHORT_BREAK,
+  LONG_BREAK,
+]
+
+
 function App() {
   const [timer, setTimer] = useState(STARTING_SECONDS)
+  const [maxTime, setMaxTime] = useState(STARTING_SECONDS)
+  const [startingTime, setStartingTime] = useState(undefined)
   const [isStarted, setIsStarted] = useState(false)
   const [runCount, setRunCount] = useState(0)
 
@@ -18,39 +29,68 @@ function App() {
   // If you have fewer than four checkmarks, take a short break (3–5 minutes) and then return to step 2; otherwise continue to step 6.
   // After four pomodoros, take a longer break (15–30 minutes), reset your checkmark count to zero, then go to step 1.
 
-  useEffect(() => {
-    const breakDurationSequence = [
-      SHORT_BREAK,
-      SHORT_BREAK,
-      SHORT_BREAK,
-      LONG_BREAK,
-    ]
+  const resetTick = useCallback(() => {
+    setStartingTime(Temporal.now.instant())
+  }, [setStartingTime])
 
-    const runTimer = () => {
-      setTimeout(() => {
-        setTimer(timer - 1)
 
-        if (timer < 1) {
-          setIsStarted(false)
-          // check if number is odd
-          if (runCount % 2 !== 0) {
-            setRunCount(runCount + 1)
-            setTimer(STARTING_SECONDS)
-          } else if (runCount === 9) {
-            setTimer(STARTING_SECONDS)
-            setRunCount(0)
-          } else {
-            setRunCount(runCount + 1)
-            setTimer(breakDurationSequence[Math.floor(runCount / 2)])
-          }
-        }
-      }, 1000)
+  const setTimerLength = useCallback(() => {
+    // check if number is odd
+    if (runCount % 2 !== 0) {
+      setRunCount(runCount + 1)
+      setTimer(STARTING_SECONDS)
+      setMaxTime(STARTING_SECONDS)
+    } else if (runCount === 9) {
+      setTimer(STARTING_SECONDS)
+      setMaxTime(STARTING_SECONDS)
+
+      setRunCount(0)
+    } else {
+      setRunCount(runCount + 1)
+      const maximum = breakDurationSequence[Math.floor(runCount / 2)]
+      setTimer(maximum)
+      setMaxTime(maximum)
     }
 
+    resetTick()
+  }, [setTimer, setRunCount, runCount, resetTick])
+
+  const runTimer = useCallback(() => {
+    if (!startingTime) {
+      resetTick()
+    }
+
+    const getElapsedTime = () => {
+      if (!startingTime) {
+        return -1
+      }
+      const time = Temporal.now.instant().since(startingTime)
+      return maxTime - time.seconds
+    }
+
+    const tickTock = setInterval(() => {
+      if (startingTime) {
+        const time = getElapsedTime()
+
+       const shouldAdvance =  timer - 1 === time
+
+        if (shouldAdvance) {
+          setTimer(timer - 1)
+        }
+      }
+
+      if (timer < 1) {
+        setTimerLength()
+        clearInterval(tickTock)
+      }
+    }, 100)
+  }, [startingTime, resetTick, timer, maxTime, setTimerLength])
+
+  useEffect(() => {
     if (isStarted) {
       runTimer()
     }
-  }, [setTimer, timer, isStarted, setIsStarted, runCount, setRunCount])
+  }, [runTimer, isStarted])
 
   return (
     <div className="App">
